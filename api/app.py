42cd2fe93408ad11d7b0e2d3819a22e4fc96b16b
ozaki58 <ozaki.shun.20@shizuoka.ac.jp>
2023-12-08 21:17:39 +0900

    ユーザー情報関連API作成

--- api/app.py
+++ api/app.py
@@ -5,12 +5,14 @@ from datetime import datetime
 
 from function.goals.routes import goals_blueprint
 from function.quests.routes import quests_blueprint
+from function.users.routes import users_blueprint
 
 app = Flask(__name__)
 
 
 app.register_blueprint(goals_blueprint)
 app.register_blueprint(quests_blueprint)
+app.register_blueprint(users_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/function/users/routes.py
+++ api/function/users/routes.py
@@ -7,3 +7,40 @@ from flask import Blueprint, jsonify
 
 users_blueprint = Blueprint('users', __name__)
 
+# ユーザーIDに紐づいたユーザー情報取得
+@users_blueprint.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    file_path = os.path.join(current_app.root_path, 'data', 'users.json')
+    
+    with open(file_path, 'r', encoding="utf-8") as f:
+        users_data = json.load(f)
+    
+    # user_idに基づいてusersをフィルタリングする
+    filtered_user = [user for user in users_data["users"] if user["id"] == user_id]
+    
+    user_json = json.dumps(filtered_user, ensure_ascii=False,indent=2)
+    
+    return Response(user_json, content_type='application/json; charset=utf-8')
+
+
+#ユーザー情報を更新
+@users_blueprint.route('/users/<int:user_id>', methods=['PATCH'])
+def update_user(user_id):
+
+    user_data = request.json
+    if not user_data:
+        return jsonify({"error": "No data provided"}), 400
+
+    # 簡易的な実装：与えられたデータを返すだけ
+    updated_user = {
+        "id": user_id,
+        "name": user_data.get('name', 'No name provided'),
+        "email": user_data.get('email', 'No email provided')
+    }
+    
+
+    user_json = json.dumps(updated_user, ensure_ascii=False,indent=2)
+    
+    return Response(user_json, content_type='application/json; charset=utf-8')
+ 
+    
