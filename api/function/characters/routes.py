feed9e95baefcb19c3a4e6edc20fd293006c6efb
ozaki58 <ozaki.shun.20@shizuoka.ac.jp>
2023-12-13 16:53:31 +0900

    キャラクターお気に入り登録API追加

--- api/function/characters/routes.py
+++ api/function/characters/routes.py
@@ -48,7 +48,23 @@ def unlock(user_id,character_id):
     
     return Response(filtered_json, content_type='application/json; charset=utf-8')
     
+#　お気に入りキャラクター設定
+@characters_blueprint.route('/users/<int:user_id>/characters/<int:character_id>/favor', methods=['PATCH'])
+def favor(user_id,character_id):
+    file_path = os.path.join(current_app.root_path, 'data', 'characters_status.json')
+    with open(file_path, 'r', encoding="utf-8") as f:
+        users_characters= json.load(f)
+    filtered_users_characters = [users_characters for users_characters in users_characters['characters_status'] if users_characters['user_id'] == user_id and users_characters['character_id'] == character_id]
+    if filtered_users_characters:
+        filtered_users_characters[0]['is_favored'] = "True"
+    else: 
+        return("指定されたキャラクターIDとユーザーIDを持つようなデータはない")
 
+
+    filtered_json = json.dumps(filtered_users_characters, ensure_ascii=False,indent=2)
+    
+    return Response(filtered_json, content_type='application/json; charset=utf-8')
+    
 
 
 
