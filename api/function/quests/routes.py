b3017a8dfd3c4ae788c72999151e0ca84e279450
ozaki58 <ozaki.shun.20@shizuoka.ac.jp>
2023-12-15 07:40:48 +0900

    クエストの達成とコインの取得APIを追加

--- api/function/quests/routes.py
+++ api/function/quests/routes.py
@@ -34,7 +34,7 @@ def get_quest(user_id,goal_id,quest_id):
     
     quest = [quest for quest in data["quests"] if quest["user_id"] == user_id and quest["goal_id"] == goal_id and quest["id"] == quest_id]
     
-    quest_json = json.dumps(quest, ensure_ascii=False,indent=2)
+    quest_json = json.dumps(quest[0], ensure_ascii=False,indent=2)
     
     return Response(quest_json, content_type='application/json; charset=utf-8')
    
@@ -43,7 +43,7 @@ def get_quest(user_id,goal_id,quest_id):
 @quests_blueprint.route('/users/<int:user_id>/goals/<int:goal_id>/quests/<int:quest_id>', methods=['PATCH'])
 def quest_edit(user_id,goal_id,quest_id):
     
-    req_quest_data = request.json
+    
     
     file_path = os.path.join(current_app.root_path, 'data', 'quests.json')
     with open(file_path, 'r', encoding="utf-8") as f:
@@ -70,8 +70,54 @@ def quest_edit(user_id,goal_id,quest_id):
     if new_end_date:
         filtered_quest[0]["end_date"] = new_end_date
 
-    updated_data = filtered_quest
+    updated_data = filtered_quest[0]
     data_json = json.dumps(updated_data, ensure_ascii=False,indent=2)
     
     return Response(data_json, content_type='application/json; charset=utf-8')
 
+
+ #クエストの完了とコインの取得
+@quests_blueprint.route('/users/<int:user_id>/goals/<int:goal_id>/quests/<int:quest_id>/complete', methods=['PATCH']) 
+def quest_complete(user_id,goal_id,quest_id):
+
+    file_quests_path = os.path.join(current_app.root_path, 'data', 'quests.json')
+    file_coins_path = os.path.join(current_app.root_path, 'data', 'coins.json')
+    
+    with open(file_quests_path, 'r', encoding="utf-8") as f:
+        quests_data = json.load(f)
+    with open(file_coins_path, 'r', encoding="utf-8") as f:
+        coins_data = json.load(f)
+
+    new_status = request.json.get('status')
+
+    #questデータをフィルタリング
+    try:
+        filtered_quest = [quest for quest in quests_data["quests"] if quest["user_id"] == user_id and quest["goal_id"] == goal_id and quest["id"] == quest_id]
+    
+    except:
+        return jsonify({"error": "指定されたデータは存在しない"}), 400
+    
+    #coinsデータをuser_idでフィルタリング
+    try:
+        filtered_coin = [coin for coin in coins_data["coins"] if coin["user_id"] == user_id]
+    except:
+        return jsonify({"error": "そのユーザーのコインデータは存在しない"}), 400
+    
+    try:
+        if new_status == 100:
+            filtered_quest[0]["status"] = new_status
+            filtered_coin[0]["amount"] += 100  # 報酬を100コインとしている
+        else:
+            return jsonify({"error": "クエストはまだ完了していません"}), 400
+
+    except IndexError:
+        # フィルタリングされたリストが空の場合のエラーハンドリング
+        return jsonify({"error": "指定されたクエストまたはコインが見つかりません"}), 404
+    
+    updated_data = {
+        "updated_quest":filtered_quest[0],
+        "updated_coin":filtered_coin[0]
+    }
+    data_json = json.dumps(updated_data, ensure_ascii=False,indent=2)
+        
+    return Response(data_json, content_type='application/json; charset=utf-8')
