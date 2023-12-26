52a1b352b9cf05e03d1eb8bff4f26ad939f63198
ozaki58 <ozaki.shun.20@shizuoka.ac.jp>
2023-12-27 01:46:42 +0900

    クエストリストの取得やクエストの詳細更新APIをデータベースと連携させた

--- api/function/quests/routes.py
+++ api/function/quests/routes.py
@@ -3,77 +3,117 @@ import json
 import os
 from datetime import datetime
 from flask import Blueprint, jsonify
-
+import pymysql.cursors
 quests_blueprint = Blueprint('quests', __name__)
+
+#データベース設定
+def get_db_connection():
+    return pymysql.connect(host='localhost',
+                    user='root',
+                    db='hackathon_project',
+                    charset='utf8mb4',
+                    password='ozaki',
+                    cursorclass=pymysql.cursors.DictCursor)
 
 #目標に紐づくクエストリストの取得
 
 @quests_blueprint.route('/users/<int:user_id>/goals/<int:goal_id>/quests', methods=['GET'])
-def get_quests_for_goal(user_id, goal_id):
-
-    file_path = os.path.join(current_app.root_path, 'data', 'quests.json')
+def get_quests_for_goal(user_id,goal_id):
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            # user_goal、goal_quests、および quests テーブルを結合する
+            cursor.execute('''
+                SELECT q.* FROM quests AS q
+                INNER JOIN goal_quests AS gq ON q.id = gq.quest_id
+                INNER JOIN user_goals AS ug ON gq.goal_id = ug.goal_id
+                WHERE ug.user_id = %s AND ug.goal_id = %s
+            ''', (user_id, goal_id))
+            quests = cursor.fetchall()
+        return jsonify(quests)
+    finally:
+        conn.close()
     
-    with open(file_path, 'r', encoding="utf-8") as f:
-        quests_data = json.load(f)
     
-    # goal_idに基づいてクエストをフィルタリングする
-    filtered_quests = [quest for quest in quests_data["quests"] if quest["goal_id"] == goal_id and quest["user_id"] == user_id]
-    
-    quests_json = json.dumps(filtered_quests, ensure_ascii=False,indent=2)
-    
-    return Response(quests_json, content_type='application/json; charset=utf-8')
 
 #クエストの詳細を取得
 @quests_blueprint.route('/quests/<int:quest_id>', methods=['GET'])
 def get_quest(quest_id):
     
-    file_path = os.path.join(current_app.root_path, 'data', 'quests.json')
-    
-    with open(file_path, 'r', encoding="utf-8") as f:
-        data = json.load(f)
-    
-    quest = [quest for quest in data["quests"] if quest['id'] == quest_id]
-    
-    quest_json = json.dumps(quest[0], ensure_ascii=False,indent=2)
-    
-    return Response(quest_json, content_type='application/json; charset=utf-8')
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute('SELECT * FROM quests WHERE id = %s', (quest_id,))
+            quest = cursor.fetchone()
+        return jsonify(quest) if quest else ('', 404)
+    finally:
+        conn.close()
 
 #クエストの詳細編集
 @quests_blueprint.route('/users/<int:user_id>/goals/<int:goal_id>/quests/<int:quest_id>', methods=['PATCH'])
-def quest_edit(user_id,goal_id,quest_id):
-    
-    
-    
-    file_path = os.path.join(current_app.root_path, 'data', 'quests.json')
-    with open(file_path, 'r', encoding="utf-8") as f:
-        quests_data = json.load(f)
+def quest_edit(user_id, goal_id, quest_id):
+    # リクエストから更新するデータを取得
     new_title = request.json.get('title')
     new_description = request.json.get('description')
     new_status = request.json.get('status')
     new_end_date = request.json.get('end_date')
 
-    #フィルタリング
+    # データベース接続を開始
+    conn = get_db_connection()
     try:
-        filtered_quest = [quest for quest in quests_data["quests"] if quest["user_id"] == user_id and quest["goal_id"] == goal_id and quest["id"] == quest_id]
-    
-    except:
-        return jsonify({"error": "指定されたデータは存在しない"}), 400
-    
-    #詳細を変更
-    if new_title:
-        filtered_quest[0]["title"] = new_title
-    if new_description:
-        filtered_quest[0]["description"] = new_description
-    if new_status:
-        filtered_quest[0]["status"] = new_status
-    if new_end_date:
-        filtered_quest[0]["end_date"] = new_end_date
+        with conn.cursor() as cursor:
+            # クエリのパーツを格納するリスト
+            update_parts = []
+            update_values = []
+            
+            # 各フィールドについて更新する値があれば、クエリのパーツを追加
+            if new_title:
+                update_parts.append('q.title = %s')
+                update_values.append(new_title)
+            if new_description:
+                update_parts.append('q.description = %s')
+                update_values.append(new_description)
+            if new_status is not None:
+                update_parts.append('q.status = %s')
+                update_values.append(new_status)
+            if new_end_date:
+                update_parts.append('q.end_date = %s')
+                update_values.append(new_end_date)
+            
+            # 更新する値がある場合にのみクエリを実行
+                
+            # 更新する値がある場合にのみクエリを実行
+            if update_parts:
+                # クエリの文字列を組み立て
+                update_string = ', '.join(update_parts)
+                update_values.append(user_id)
+                update_values.append(goal_id)
+                update_values.append(quest_id)
 
-    updated_data = filtered_quest[0]
-    data_json = json.dumps(updated_data, ensure_ascii=False,indent=2)
-    
-    return Response(data_json, content_type='application/json; charset=utf-8')
+                # クエリを実行
+                cursor.execute(f'''
+                    UPDATE quests q
+                    INNER JOIN goal_quests gq ON q.id = gq.quest_id
+                    INNER JOIN user_goals ug ON gq.goal_id = ug.goal_id
+                    SET {update_string}
+                    WHERE ug.user_id = %s AND ug.goal_id = %s AND q.id = %s
+                ''', update_values)
 
+                
+                conn.commit()
+
+                # 更新されたクエストを取得
+                cursor.execute('SELECT * FROM quests WHERE id = %s', (quest_id))
+                updated_quest = cursor.fetchone()
+                return jsonify(updated_quest) if updated_quest else ('', 404)
+            else:
+                return jsonify({"error": "No update parameters provided"}), 400
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
+        conn.rollback()
+        return jsonify({"error": "Database error"}), 500
+    finally:
+        conn.close()
 
  #クエストの完了とコインの取得
 @quests_blueprint.route('/users/<int:user_id>/goals/<int:goal_id>/quests/<int:quest_id>/complete', methods=['PATCH']) 
